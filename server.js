6b6e86dfe0cf8561c125490cf75ba120900ebb88
Kingsten Banh <kingsten.banh@gmail.com>
2015-02-05 14:39:03 -0700

    set up Express server

---
+++ server.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var router = require('./server/routes.js');
+var connection = require('./server/connection.js');
+
+var app = express();
+var port = process.env.PORT || 8080;
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// apply the routes to our application
+app.use('/api', router);
+
+app.listen(port, function() {
+  console.log('Express server listening on port ', port);
+});
