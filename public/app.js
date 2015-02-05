664df885c82003ad67fd31bf1d0cb509d293bb03
Kingsten Banh <kingsten.banh@gmail.com>
2015-02-05 15:58:57 -0700

    add client-site routes configuration

---
+++ public/app.js
@@ -0,0 +1,29 @@
+'use strict';
+
+angular
+  .module('app', [
+    'ui.router'
+  ])
+  .config(_config);
+
+_config.$inject = ['$stateProvider', '$urlRouterProvider', '$locationProvider'];
+
+function _config($stateProvider, $urlRouterProvider, $locationProvider) {
+  $locationProvider.html5Mode(true);
+
+  // set up routes
+  $stateProvider
+    .state('home', {
+      url: '/',
+      templateUrl: 'app/search/search.html',
+      controller: 'Search'
+    })
+    .state('result', {
+      url: '/results',
+      templateUrl: 'app/results/results.html',
+      controller: 'Results'
+    });
+
+  // Redirect back to home page for other routes
+  $urlRouterProvider.otherwise('/');
+}
